c51094ca6ccf48ab7630d72e00bc333dab04257b
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2023-11-30 22:22:21 +0600

    Create ES6_01_Syllabus.js

---
+++ ES6_01_Syllabus.js
@@ -0,0 +1,38 @@
+01 🔥Let & Const
+
+🐦 Rest Parameter 
+🐦 Spread Operator
+🐦 for .. of
+🐦 object literal 
+
+02 🔥 Arrow Function 
+
+03 🔥Destructuring
+👌Array Destructuring
+👌Object Destructuring
+
+04 🔥Class
+
+05🔥 Iterators
+
+06 🔥Generators
+
+------ yield
+
+
+07 🔥 Promises 
+
+08 🔥Map
+
+
+----- Set
+
+🔥 Array Extension 
+🔥 String Extension 
+🔥 Object Extension 
+
+🔥 Proxy
+🔥 Reflection 
+
+
+  
