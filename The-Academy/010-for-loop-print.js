f8cb0baabe839c0625b58a20250ecc6b0e593f15
MD. LINGKON ISLAM <111384782+lingkon-record@users.noreply.github.com>
2024-05-22 19:56:54 +0600

    Create 010-for-loop-print.js

---
+++ The-Academy/010-for-loop-print.js
@@ -0,0 +1,13 @@
+ for ( let start = 50; start <= 55; start++){
+    // console.log(start);
+    // console.log("I am good one");
+}
+
+// next 
+let start1 = 10
+let end2 = 20
+for (start1; start1 <= end2; start1++){
+    console.log(start1);
+    // console.log("Nirob-" + start1);
+
+}
